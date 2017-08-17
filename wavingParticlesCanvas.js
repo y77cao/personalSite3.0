89d1c77aa45b7b0d93864bcd042facce52a3e43d
LOYCORP\YCAO <ycao@loyalty.com>
2017-08-17 16:45:20 -0400

    too lazy...thats it for today

--- wavingParticlesCanvas.js
+++ wavingParticlesCanvas.js
@@ -5,10 +5,12 @@
 			var mouseX = 0, mouseY = 0;
 			var windowHalfX = window.innerWidth / 2;
 			var windowHalfY = window.innerHeight / 2;
+
 			init();
 			animate();
+
 			function init() {
-				container = document.createElement( 'div' );
+				container = document.getElementByClassName('canvas');
 				document.body.appendChild( container );
 				camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
 				camera.position.z = 1000;
@@ -23,6 +25,7 @@
 						context.fill();
 					}
 				} );
+
 				var i = 0;
 				for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
 					for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
@@ -42,6 +45,7 @@
 				//
 				window.addEventListener( 'resize', onWindowResize, false );
 			}
+
 			function onWindowResize() {
 				windowHalfX = window.innerWidth / 2;
 				windowHalfY = window.innerHeight / 2;
@@ -49,11 +53,13 @@
 				camera.updateProjectionMatrix();
 				renderer.setSize( window.innerWidth, window.innerHeight );
 			}
+
 			//
 			function onDocumentMouseMove( event ) {
 				mouseX = event.clientX - windowHalfX;
 				mouseY = event.clientY - windowHalfY;
 			}
+
 			function onDocumentTouchStart( event ) {
 				if ( event.touches.length === 1 ) {
 					event.preventDefault();
@@ -61,6 +67,7 @@
 					mouseY = event.touches[ 0 ].pageY - windowHalfY;
 				}
 			}
+
 			function onDocumentTouchMove( event ) {
 				if ( event.touches.length === 1 ) {
 					event.preventDefault();
@@ -73,6 +80,7 @@
 				requestAnimationFrame( animate );
 				render();
 			}
+
 			function render() {
 				camera.position.x += ( mouseX - camera.position.x ) * .05;
 				camera.position.y += ( - mouseY - camera.position.y ) * .05;
